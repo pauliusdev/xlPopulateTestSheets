b559871346029149249dacbfca3dea9eed442f3b
pauliusdev <pauliusbucks@gmail.com>
2021-11-11 15:50:52 +0000

    few steps towards the end goal..

---
+++ Populate.py
@@ -0,0 +1,29 @@
+from openpyxl import Workbook,load_workbook
+from openpyxl.chart import AreaChart,Reference
+from openpyxl.utils import get_column_letter
+import random
+
+# opens file 
+workbook = load_workbook(filename="test.xlsx")
+
+workbook = Workbook()
+sheet = workbook.active
+
+rows = [ 
+    (None,None,random.randint(1,100),random.randint(1,100)),
+    (None,None,random.randint(1,100),random.randint(1,100)),
+    (None,None,random.randint(1,100),random.randint(1,100)),
+    (None,None,random.randint(1,100),random.randint(1,100)),
+    (None,None,random.randint(1,100),random.randint(1,100)),
+    (None,None,random.randint(1,100),random.randint(1,100)),
+    (None,None,random.randint(1,100),random.randint(1,100)),
+]
+  
+
+# writes data for the rows above
+for row in rows:
+    sheet.append(row)
+
+
+# saves file
+workbook.save(filename="hello.xlsx")
